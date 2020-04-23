3f71491a68e4827edce5e3d1cc5620e4cc9c8937
Matshwenyego_II <Ashleyngoetjana@gmail.com>
2020-04-23 11:49:03 +0200

    adding users type to graphqlfunction

--- server.js
+++ server.js
@@ -1,25 +1,52 @@
 const { graphql, buildSchema } = require("graphql");
 
+const db = {
+  users: [
+    {
+      id: "1",
+      email: "gideon@gmail.com",
+      name: "Gideon",
+      avatarUrl: "http://avatar.com/..."
+    },
+    {
+      id: "2",
+      email: "max@gmail.com",
+      name: "Max",
+      avatarUrl: "http://avatar.com/..."
+    }
+  ]
+};
+
 const schema = buildSchema(`
   type Query {
-    message: String
+    users: [User!]!
   }
+  type User {
+  id: ID!
+  email: String!
+  name: String
+  avatarUrl : String
+}
 
 
   `);
 
 const rootValue = {
-  message: () => "graphql works"
+  users: () => db.users
 };
 
 graphql(
   schema,
   `
       {
-        message
+        users {
+          email
+          name
+          avatarUrl
+        }
       }
     `,
   rootValue
 )
-  .then(res => console.log(res))
+  .then(res => console.dir(res.data, { depth: null }))
   .catch(console.error);
